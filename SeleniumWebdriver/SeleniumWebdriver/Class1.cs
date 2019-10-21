003ca5f78fdd352b5019852acd0ab459815c9154
Anushka <anushka@gmail.com>
2019-10-21 12:44:37 +0530

    UI testing added

---
+++ SeleniumWebdriver/SeleniumWebdriver/Class1.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver
+{
+   [TestFixture]
+    public class Class1 : TestBase
+    {
+        [Test]
+        public void verifyValidLogin()
+        {
+            try
+            {
+                driver.Url = "C:\\Users\\Admin\\source\\repos\\devopsFrontendLogin\\index.html";
+                driver.FindElement(By.Id("name")).SendKeys("anushka");
+                driver.FindElement(By.Id("password")).SendKeys("anushka");
+                driver.FindElement(By.Id("submitForm")).Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                Assert.That(alert.Text, Does.Match("User successfully logged in"));
+                alert.Accept();
+            }
+            catch (NoAlertPresentException)
+            {
+                
+            }
+        }
+
+        
+        [Test]
+        public void verifyInvalidLogin()
+        {
+            try
+            {
+              driver.Url = "C:\\Users\\Admin\\source\\repos\\devopsFrontendLogin\\index.html";
+              driver.FindElement(By.Id("name")).SendKeys("anushka123");
+              driver.FindElement(By.Id("password")).SendKeys("anushka123");
+              driver.FindElement(By.Id("submitForm")).Click();
+              IAlert alert = driver.SwitchTo().Alert();
+              Assert.That(alert.Text, Does.Match("Incorrect username or password"));
+              alert.Accept();
+            }
+            catch (NoAlertPresentException)
+            {
+
+            }
+        }
+    }
+}
---
+++ SeleniumWebdriver/SeleniumWebdriver/TestBase.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver
+{
+    public class TestBase
+    {
+
+        protected IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\Admin\\Downloads\\chromedriver_win32");
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Quit();
+        }
+
+    }
+}
